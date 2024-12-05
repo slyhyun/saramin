dbe6f7ab33d2c28a6d2d7e8de57e609de78c43fb
slyhyun <lipsul061722@jbnu.ac.kr>
2024-12-05 21:13:04 +0900

    Initial commit

---
+++ src/main/java/com/wsd/saramin/SaraminApplication.java
@@ -0,0 +1,13 @@
+package com.wsd.saramin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaraminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaraminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wsd/saramin/SaraminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsd.saramin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaraminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
