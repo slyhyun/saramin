f57ead4e4704dfa3941d0eeee912ac34da9242ec
slyhyun <lipsul061722@jbnu.ac.kr>
2024-12-08 01:28:29 +0900

    Job DTO

---
+++ src/main/java/com/wsd/saramin/job/dto/JobDTO.java
@@ -0,0 +1,62 @@
+package com.wsd.saramin.job.dto;
+
+import com.wsd.saramin.company.entity.Company;
+import com.wsd.saramin.job.entity.Job;
+import com.wsd.saramin.user.entity.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class JobDTO {
+    private long jobId;
+    private String title;
+    private String location;
+    private String experience;
+    private String type;
+    private String education;
+    private String deadline;
+    private String sector;
+    private long view;
+    private LocalDateTime date;
+
+    private String companyName; // Company 이름만 포함
+    private String userName;    // User 이름만 포함
+
+    // Entity → DTO 변환 생성자
+    public JobDTO(Job job) {
+        this.jobId = job.getJobId();
+        this.title = job.getTitle();
+        this.location = job.getLocation();
+        this.experience = job.getExperience();
+        this.type = job.getType();
+        this.education = job.getEducation();
+        this.deadline = job.getDeadline();
+        this.sector = job.getSector();
+        this.view = job.getView();
+        this.date = job.getDate();
+
+        this.companyName = job.getCompany().getName();
+        this.userName = job.getUser().getName();
+    }
+
+    // DTO → Entity 변환 메서드
+    public Job toEntity(Company company, User user) {
+        Job job = new Job();
+        job.setTitle(this.title);
+        job.setLocation(this.location);
+        job.setExperience(this.experience);
+        job.setType(this.type);
+        job.setEducation(this.education);
+        job.setDeadline(this.deadline);
+        job.setSector(this.sector);
+
+        // 연관 엔티티 설정
+        job.setCompany(company);
+        job.setUser(user);
+
+        return job;
+    }
+}
