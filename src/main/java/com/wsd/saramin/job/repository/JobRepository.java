e3085d00e5514f6a9540c3f18a71717ab13f9545
slyhyun <lipsul061722@jbnu.ac.kr>
2024-12-09 20:23:32 +0900

    Job Repository

---
+++ src/main/java/com/wsd/saramin/job/repository/JobRepository.java
@@ -0,0 +1,10 @@
+package com.wsd.saramin.job.repository;
+
+import com.wsd.saramin.job.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long>, JpaSpecificationExecutor<Job> {
+}
