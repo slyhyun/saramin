eb34516cd1eaa78a6c34f0293c635b15ff58597d
slyhyun <lipsul061722@jbnu.ac.kr>
2024-12-11 20:03:46 +0900

    Company Repository

---
+++ src/main/java/com/wsd/saramin/company/repository/CompanyRepository.java
@@ -0,0 +1,9 @@
+package com.wsd.saramin.company.repository;
+
+import com.wsd.saramin.company.entity.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+}
