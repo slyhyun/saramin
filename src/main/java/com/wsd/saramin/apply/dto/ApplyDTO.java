79650464aba31447d87efb499246d4b6efa122b0
slyhyun <lipsul061722@jbnu.ac.kr>
2024-12-13 20:04:50 +0900

    Apply DTO

--- src/main/java/com/wsd/saramin/apply/dto/ApplyDTO.java
+++ src/main/java/com/wsd/saramin/apply/dto/ApplyDTO.java
@@ -10,20 +10,20 @@ import java.time.LocalDateTime;
 @Setter
 public class ApplyDTO {
 
-    private Long applicationId; // 기존 id를 더 명확히 변경
+    private Long applyId; // 기존 id를 더 명확히 변경
     private Long userId;
     private Long jobId;
     private String jobTitle;
-    private LocalDateTime applicationDate; // 기존 appliedAt에서 변경
+    private LocalDateTime applyDate; // 기존 appliedAt에서 변경
     private String status;
 
     // Entity → DTO 변환 생성자
-    public ApplyDTO(Apply application) {
-        this.applicationId = application.getApplyId(); // 변경된 엔티티 메서드
-        this.userId = application.getUser().getUserId();
-        this.jobId = application.getJob().getJobId();
-        this.jobTitle = application.getJob().getTitle();
-        this.applicationDate = application.getDate(); // appliedAt → date로 변경
-        this.status = application.getStatus().name();
+    public ApplyDTO(Apply apply) {
+        this.applyId = apply.getApplyId(); // 변경된 엔티티 메서드
+        this.userId = apply.getUser().getUserId();
+        this.jobId = apply.getJob().getJobId();
+        this.jobTitle = apply.getJob().getTitle();
+        this.applyDate = apply.getDate(); // appliedAt → date로 변경
+        this.status = apply.getStatus().name();
     }
 }
