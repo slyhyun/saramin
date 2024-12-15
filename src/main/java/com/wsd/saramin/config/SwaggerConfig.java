0901e852e52cf192486bb916b6ae3ca27e42075f
slyhyun <lipsul061722@jbnu.ac.kr>
2024-12-15 11:08:40 +0900

    Swagger Config

--- src/main/java/com/wsd/saramin/config/SwaggerConfig.java
+++ src/main/java/com/wsd/saramin/config/SwaggerConfig.java
@@ -1,20 +1,30 @@
 package com.wsd.saramin.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
+
     @Bean
-    public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.wsd.saramin"))
-                .paths(PathSelectors.any())
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Saramin Application API")
+                        .version("1.0.0")
+                        .description("Saramin 지원 API 문서입니다.")
+                        .contact(new Contact()
+                                .name("Saramin Support")
+                                .email("support@saramin.com")))
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("로컬 환경"),
+                        new Server().url("https://api.saramin.com").description("운영 환경")
+                ));
     }
 }
