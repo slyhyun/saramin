73c36e3d77e0cd6fbcddec87d5725ad5fc45e469
slyhyun <lipsul061722@jbnu.ac.kr>
2024-12-16 17:07:25 +0900

    Cors Config

---
+++ src/main/java/com/wsd/saramin/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.wsd.saramin.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*"); // 모든 Origin 허용
+        config.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+        config.addAllowedHeader("*"); // 모든 헤더 허용
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
